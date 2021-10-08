2e05cc0e3e5ee8678809f5ce63126dbe5e4e8b30
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-10-08 19:53:28 +0400

    fix tests

--- tests/test/test_console.py
+++ tests/test/test_console.py
@@ -5,14 +5,30 @@ make test T=test_console.py
 from . import TestBase
 
 
+def stub_com1_bluetooth():
+    """Return one comport."""
+    return [('COM1', 'Bluetooth port COM1', 1)]
+
+
+def stub_com45_bluetooth():
+    """Return two comports."""
+    return [
+      ('COM4', 'Bluetooth port COM4', 1),
+      ('COM5', 'Bluetooth port COM5', 2),
+    ]
+
+
 class TestConsole(TestBase):
     """Tests console client."""
 
     def test_no_elm(self):
         """Call app without ELM device."""
-        from source.main import main
+        from source import main
+        saved = main.comports
 
-        assert main([], self.options) == 1
+        main.comports = stub_com1_bluetooth
+        assert main.main([], self.options) == 1
+        main.comports = saved
 
     def test_no_ports(self):
         """Call app without COM ports."""
