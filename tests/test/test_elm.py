d6a1931b217c0908d03784fe17a70214fea35622
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-10-10 21:18:17 +0400

    coverage

--- tests/test/test_elm.py
+++ tests/test/test_elm.py
@@ -2,7 +2,25 @@
 
 make test T=test_elm.py
 """
+import serial
+from tests.mock.serial import Port
 from . import TestBase
+
+
+class MockNotWriteble(Port):
+    """Mocked not writeble serial port."""
+
+    def write(self, _bytes_array):
+        """No write."""
+        raise serial.serialutil.SerialTimeoutException(self.port)
+
+
+class MockElm(Port):
+    """Mocked serial port with ELM device."""
+
+    def read(self, _bytes_num):
+        """Read given bytes number from serial port."""
+        return b'ATZ\r\r\rELM327 v1.5\r\r'
 
 
 class TestElm(TestBase):
@@ -23,3 +41,18 @@ class TestElm(TestBase):
         assert not name_from_response(b'')
         assert not name_from_response(b'xxx')
         assert name_from_response(b'ATZ\r\r\rELM327 v1.5\r\r') == "ELM327 v1.5"
+
+    @staticmethod
+    def test_at_port_wrong():
+        """Class method at_port."""
+        from source.elm import Device, serial as mockserial
+
+        saved = mockserial.Serial
+        mockserial.Serial = MockNotWriteble
+
+        assert Device.at_port('COM1', 38400) is None
+
+        mockserial.Serial = MockElm
+        assert Device.at_port('COM1', 38400).name == 'ELM327 v1.5'
+
+        mockserial.Serial = saved
