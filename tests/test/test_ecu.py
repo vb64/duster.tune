b358bb107f8c5afc242fd5956985381db53133da
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2021-10-12 18:26:41 +0400

    tests

--- tests/test/test_ecu.py
+++ tests/test/test_ecu.py
@@ -8,14 +8,25 @@ from . import TestBase
 class TestEcu(TestBase):
     """Tests Ecu database."""
 
-    def test_database(self):
-        """Database class."""
+    def test_database_2018(self):
+        """Database class from ecu_2018.zip."""
+        from source.ecu import Database
+        from source.vehicles import DATA
+
+        ecu_db = Database(self.fixture('ecu_2018.zip'), DATA)
+        assert ecu_db.count == 1835
+        assert len(ecu_db.unknown_vehicles) == 60
+        assert len(ecu_db.vehiclemap) == 77
+        assert len(ecu_db.protocol_kwp) == 28
+        assert len(ecu_db.protocol_can) == 103
+        assert len(ecu_db.protocol_unknown) == 1
+
+    def test_database_2019(self):
+        """Database class from ecu_2019.zip."""
         from source.ecu import Database
         from source.vehicles import DATA
 
         assert len(DATA) == 80
-        # ecu_db = Database(self.fixture('ecu_2018.zip'))
-        # assert ecu_db.numecu == 1835
         ecu_db = Database(self.fixture('ecu_2019.zip'), DATA)
         assert ecu_db.count == 3478
         assert len(ecu_db.unknown_vehicles) == 77
